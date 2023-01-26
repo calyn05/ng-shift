b7c952da78f8eaf25a3c900ea7916476db78f2ec
calyn05 <67522453+calyn05@users.noreply.github.com>
2023-01-26 13:20:59 +0200

    first commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'calin-codes',
+    appId: '1:741989930730:web:9fc69a452d23e8302da993',
+    storageBucket: 'calin-codes.appspot.com',
+    locationId: 'europe-central2',
+    apiKey: 'AIzaSyAl6xJE03kd97mLT7E-vG2mnV0wxDTW-mk',
+    authDomain: 'calin-codes.firebaseapp.com',
+    messagingSenderId: '741989930730',
+  },
   production: true
 };
